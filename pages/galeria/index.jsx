fa0760ee6796e9988703d7a39fa32a1b9fa1be9e
Victor <gamesvictor1928@gmail.com>
2023-01-01 14:23:37 -0300

    Galeria
    Diretor4? Tchau

--- pages/galeria/index.jsx
+++ pages/galeria/index.jsx
@@ -7,7 +7,6 @@ import BielRiste from "../../public/images/Biels/BielRiste.jpg"; // Guerra civil
 import BielS2 from "../../public/images/Biels/BielS2.jpg"
 import BielSentado from "../../public/images/Biels/BielSentado.jpeg"
 
-import Diretor4 from "../../public/images/Diretores/Diretor4.jpg"
 import FernandoFerraz from "../../public/images/Diretores/FernandoFerraz.jpg"
 import FoguinhoDaRecordTV from "../../public/images/Diretores/FoguinhodaRecordTV.jpg"
 import GabrielaRosales from "../../public/images/Diretores/GabrielaRosales.jpg"
@@ -16,16 +15,12 @@ import MonicaTeixeira from "../../public/images/Diretores/MônicaTeixeira.jpg"
 import Parallax from '../../components/parallax'
 import Transitions from '../../components/transicao'
 
-import { GaleriaModelo, GaleriaDiretores } from "./style.module.scss"
+import { GaleriaModelo, GaleriaDiretores, GaleriaImg } from "./style.module.scss"
 
 export default function Galeria() {
     function mapGridArea(v, index) {
         return <div
-            style={
-                {
-                    gridArea: v.src.split("/").pop().split(".").shift()
-                }
-            }
+            style={{ gridArea: v.src.split("/").pop().split(".").shift() }}
             key={index}
         >
             <Image
@@ -33,7 +28,6 @@ export default function Galeria() {
                 layout='intrinsic'
                 objectPosition={"center center"}
                 src={v}
-            // data-inviewport="scale-in"
             />
         </div>
     }
@@ -44,22 +38,15 @@ export default function Galeria() {
                 <article className={GaleriaModelo}>
                     <div id="galeria">
                         {[
-                            // BielLiz,
                             BielS2,
                             BielRiste,
                             BielSentado,
-                            // BielOlhar,
-                            // BielaA1,
-                            // BielNé,
-                            // BielCulos,
                             Bielcruzado,
                             Bielolhar,
                             BielRio,
                         ].map(mapGridArea)}
                     </div>
-                    <h2
-                        style={{ marginLeft: 0 }}
-                    >Modelo</h2>
+                    <h2 >Modelo</h2>
                 </article>
             </section>
             <section id="diretores">
@@ -68,12 +55,39 @@ export default function Galeria() {
                     <div id="diretores">
                         {
                             [
-                                MonicaTeixeira,
-                                FernandoFerraz,
-                                FoguinhoDaRecordTV,
-                                GabrielaRosales,
-                                Diretor4
-                            ].map(mapGridArea)
+                                {
+                                    img: MonicaTeixeira,
+                                    name: "Mônica Teixeira"
+                                },
+                                {
+                                    img: FernandoFerraz,
+                                    name: "Fernando Ferraz"
+                                },
+                                {
+                                    img: FoguinhoDaRecordTV,
+                                    name: "Foguinho da Record TV"
+                                },
+                                {
+                                    img: GabrielaRosales,
+                                    name: "Gabriela Rosales"
+                                }
+                            ].map(({ img, name }, k) => {
+                                return <div
+                                    style={{ gridArea: img.src.split("/").pop().split(".").shift() }}
+                                    key={k}
+                                    className={GaleriaImg}
+                                >
+                                    <div>
+                                        <Image
+                                            objectFit="cover"
+                                            layout='intrinsic'
+                                            objectPosition={"center center"}
+                                            src={img}
+                                        />
+                                    </div>
+                                    <h3>{name}</h3>
+                                </div>
+                            })
                         }
                     </div>
                 </article>
